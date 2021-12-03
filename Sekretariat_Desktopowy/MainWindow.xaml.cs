4ef036f77a0eedf501e2b76125ff97db0655297e
R <stas.frak@gmail.com>
2021-12-03 13:13:42 +0100

    Dodawanie rekordów dziala

--- Sekretariat_Desktopowy/MainWindow.xaml.cs
+++ Sekretariat_Desktopowy/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Sekretariat_Desktopowy
             InitializeComponent();
             Update_Widok();
         }
+        //deklaracja obiektów poszcególnych osób
         public class Uczen {
 
             public string Rodzaj = "U";
@@ -62,6 +63,25 @@ namespace Sekretariat_Desktopowy
             public string Zajecia = "";
             public string DataZatrudnienia;
         }
+        public class Pracownik
+        {
+            public string Rodzaj = "P";
+
+            public string Imie = "";
+            public string DrugieImie = "";
+            public string Nazwisko = "";
+            public string NazwiskoPaniejskie = "";
+            public string ImieOjca = "";
+            public string ImieMatki = "";
+            public string DataUrodzenia;
+            public string Pesel = "";
+            public string Plec = "";
+            public string Zdjecie = "";
+
+            public string Etat = "";
+            public string Opis = "";
+            public string DataZatrudnienia;
+        }
 
         string UczenWybraneZdjecie = "";
         string NauczycielWybraneZdjecie = "";
@@ -70,7 +90,7 @@ namespace Sekretariat_Desktopowy
         //Tworzenie 3 table które składają się na "baze", są narazie puste poniewaz potem dane są dodawane funkcją .concat
         Uczen[] TableUczen = new Uczen[0];
         Nauczyciel[] TableNauczyciel = new Nauczyciel[0];
-        //Pracownik[] TableUczen = new Pracownik[0];
+        Pracownik[] TablePracownik = new Pracownik[0];
 
         //Uaktualnia Widok "Bazy"
         public void Update_Widok()
@@ -84,6 +104,11 @@ namespace Sekretariat_Desktopowy
             for (int i = 0; i < TableNauczyciel.Length; i++)
             {
                 Tab_Item_Widok_TextBox.Text = Tab_Item_Widok_TextBox.Text + TableNauczyciel[i].Imie + " " + TableNauczyciel[i].DrugieImie + " " + TableNauczyciel[i].Nazwisko + " " + TableNauczyciel[i].NazwiskoPaniejskie + " " + TableNauczyciel[i].ImieOjca + " " + TableNauczyciel[i].ImieMatki + " " + TableNauczyciel[i].DataUrodzenia + " " + TableNauczyciel[i].Pesel + " " + TableNauczyciel[i].Plec + " " + TableNauczyciel[i].DataZatrudnienia + " " + TableNauczyciel[i].PrzedmiotyNauczane + " " + TableNauczyciel[i].WychowawcaKlasa + " " + TableNauczyciel[i].Zajecia + "\n";
+            }
+            if (TablePracownik.Length == 0) { Tab_Item_Widok_TextBox.Text = Tab_Item_Widok_TextBox.Text + "Brak rekordów Pracowników\n"; };
+            for (int i = 0; i < TablePracownik.Length; i++)
+            {
+                Tab_Item_Widok_TextBox.Text = Tab_Item_Widok_TextBox.Text + TablePracownik[i].Imie + " " + TablePracownik[i].DrugieImie + " " + TablePracownik[i].Nazwisko + " " + TablePracownik[i].NazwiskoPaniejskie + " " + TablePracownik[i].ImieOjca + " " + TablePracownik[i].ImieMatki + " " + TablePracownik[i].DataUrodzenia + " " + TablePracownik[i].Pesel + " " + TablePracownik[i].Plec + " " + TablePracownik[i].DataZatrudnienia + " " + TablePracownik[i].Etat + " " + TablePracownik[i].Opis + "\n";
             }
         }    
         private void Uczen_StworzRekord_Click_1(object sender, RoutedEventArgs e)
@@ -325,6 +350,8 @@ namespace Sekretariat_Desktopowy
                 NauczycielWybraneZdjecie = "";
                 Nauczyciel_Zajecia.Text = "Zajecia z klasami";
                 Nauczyciel_WychowawcaKlasy.Text = "Wychowawca Klasy";
+                Nauczyciel_PrzedmiotyNauczane.Text = "Przedmioty Nauczane";
+                Nauczyciel_DataZatrudnienia.SelectedDate = null;
                 Nauczyciel_PlecM.IsChecked = false;
                 Nauczyciel_PlecK.IsChecked = false;
                 Nauczyciel_DataUrodzenia.SelectedDate = null;
@@ -354,7 +381,143 @@ namespace Sekretariat_Desktopowy
         }
         private void Pracownik_StworzRekord_Click1(object sender, RoutedEventArgs e)
         {
+            //Jeden Wielki Check czy pola się zgadzają
+            Pracownik_ErrorLabel.Content = "";
+            bool Check = true;
+            if (Pracownik_Imie.Text == " " || Pracownik_Imie.Text == "Imie")
+            {
+                Pracownik_Imie.Foreground = Brushes.Red;
+                Check = false;
+            }
+            else { Pracownik_Imie.Foreground = Brushes.Black; }
+            if (Pracownik_Nazwisko.Text == " " || Pracownik_Nazwisko.Text == "Nazwisko")
+            {
+                Pracownik_Nazwisko.Foreground = Brushes.Red;
+                Check = false;
+            }
+            else { Pracownik_Nazwisko.Foreground = Brushes.Black; }
+            if (Pracownik_DataUrodzenia.SelectedDate >= DateTime.Now || Pracownik_DataUrodzenia.SelectedDate == null)
+            {
+                Pracownik_DataUrodzenia.Foreground = Brushes.Red;
+                Check = false;
+            }
+            else { Pracownik_DataUrodzenia.Foreground = Brushes.Black; }
+            if (Pracownik_Pesel.Text == " " || Pracownik_Pesel.Text == "Pesel" || Pracownik_Pesel.Text.Length != 11 || Pracownik_Pesel.Text.All(char.IsDigit) != true)
+            {
+                Pracownik_Pesel.Foreground = Brushes.Red;
+                Check = false;
+            }
+            else { Pracownik_Pesel.Foreground = Brushes.Black; }
 
+            if (Pracownik_PlecM.IsChecked == false && Pracownik_PlecK.IsChecked == false)
+            {
+                Pracownik_PlecM.Foreground = Brushes.Red;
+                Pracownik_PlecK.Foreground = Brushes.Red;
+                Check = false;
+            }
+            else { Pracownik_PlecM.Foreground = Brushes.Black; Pracownik_PlecK.Foreground = Brushes.Black; }
+
+            if (Pracownik_DataZatrudnienia.SelectedDate > DateTime.Now || Pracownik_DataZatrudnienia.SelectedDate == null)
+            {
+                Pracownik_DataZatrudnienia.Foreground = Brushes.Red;
+                Check = false;
+            }
+            else { Pracownik_DataZatrudnienia.Foreground = Brushes.Black; }
+            if (Pracownik_Etat.Text == " " || Pracownik_Etat.Text == "Etat")
+            {
+                Pracownik_Etat.Foreground = Brushes.Red;
+                Check = false;
+            }
+            else { Pracownik_Etat.Foreground = Brushes.Black; }
+            if (Pracownik_Opis.Text == " " || Pracownik_Opis.Text == "Opis")
+            {
+                Pracownik_Opis.Foreground = Brushes.Red;
+                Check = false;
+            }
+            else { Pracownik_Opis.Foreground = Brushes.Black; }
+
+            //Wiadomość jesli nie
+            if (Check == false)
+            {
+                Pracownik_ErrorLabel.Content = "Brakujące lub niepoprawne dane !!!";
+            }
+            //Jesli Okej to mozna deklarowac zmienną
+            //Deklarowanie tymczasowej zmiennej "Uczen" ktora jest potem dodawana do tablicy i łączona z główną tabelą
+            if (Check)
+            {
+                Pracownik_ErrorLabel.Content = "";
+                Pracownik[] TempTablePracownik = new Pracownik[1];
+                Pracownik Temp = new Pracownik();
+                string Plec = "";
+                if (Pracownik_PlecM.IsChecked == true) { Plec = "Mężczyzna"; };
+                if (Pracownik_PlecK.IsChecked == true) { Plec = "Kobieta"; };
+                string Data;
+                Data = Nauczyciel_DataUrodzenia.ToString();
+                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                Temp.Imie = Pracownik_Imie.Text;
+                Temp.Nazwisko = Pracownik_Nazwisko.Text;
+                Temp.DataUrodzenia = Data;
+                Temp.Pesel = Pracownik_Pesel.Text;
+                Temp.Plec = Plec;
+                Temp.Zdjecie = PracownikWybraneZdjecie;
+                Temp.Opis = Pracownik_Opis.Text;
+                Temp.Etat = Pracownik_Etat.Text;
+                Temp.DataZatrudnienia = Pracownik_DataZatrudnienia.ToString();
+
+                if (Pracownik_DrugieImie.Text == "Drugie Imie") { Temp.DrugieImie = " "; }
+                else { Temp.DrugieImie = Pracownik_DrugieImie.Text; };
+
+                if (Pracownik_NazwiskoPaniejskie.Text == "Nazwisko Paniejskie") { Temp.NazwiskoPaniejskie = " "; }
+                else { Temp.NazwiskoPaniejskie = Pracownik_NazwiskoPaniejskie.Text; };
+
+                if (Pracownik_ImieOjca.Text == "Imie Ojca") { Temp.ImieOjca = " "; }
+                else { Temp.ImieOjca = Pracownik_ImieOjca.Text; };
+
+                if (Pracownik_ImieMatki.Text == "Imie Matki") { Temp.ImieMatki = " "; }
+                else { Temp.ImieMatki = Pracownik_ImieMatki.Text; };
+
+                //Łączenie tymczasowej tabeli z tabelą z "bazy danych"
+                TempTablePracownik[0] = Temp;
+                TablePracownik = TablePracownik.Concat(TempTablePracownik).ToArray();
+
+                //Wyczyszczenie Pól tekstowych i powiadomienie uzytkownika
+                Pracownik_ErrorLabel.Content = "Pomyślnie stworzono";
+                Pracownik_Imie.Text = "Imie";
+                Pracownik_DrugieImie.Text = "Drugie Imie";
+                Pracownik_Nazwisko.Text = "Nazwisko";
+                Pracownik_NazwiskoPaniejskie.Text = "Nazwisko Paniejskie";
+                Pracownik_ImieOjca.Text = "Imie Matki";
+                Pracownik_ImieMatki.Text = "Imie Ojca";
+                Pracownik_Pesel.Text = "Pesel";
+                PracownikWybraneZdjecie = "";
+                Pracownik_Opis.Text = "Opis";
+                Pracownik_Etat.Text = "Etat";
+                Pracownik_DataZatrudnienia.SelectedDate = null;
+                Pracownik_PlecM.IsChecked = false;
+                Pracownik_PlecK.IsChecked = false;
+                Pracownik_DataUrodzenia.SelectedDate = null;
+                Pracownik_Zdjecia.Source = null;
+
+                Update_Widok();
+            }
+        }
+
+        private void Pracownik_WyborZdjecia_Click(object sender, RoutedEventArgs e)
+        {
+            //Kod Otwierania Obrazu z poprzedniego programu
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = "c:\\";
+            dlg.Filter = "Image files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+            dlg.RestoreDirectory = true;
+            dlg.ShowDialog();
+
+            string selectedFileName = dlg.FileName;
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(selectedFileName);
+            bitmap.EndInit();
+            Pracownik_Zdjecia.Source = bitmap;
+            PracownikWybraneZdjecie = selectedFileName;
         }
     }
     }
